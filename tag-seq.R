16f37487d9ba7daeb5a6b7b5381122ae4cc38bf3
Ben Laufer <blaufer@ucdavis.edu>
2019-02-20 16:46:37 -0800

    Count and design matrix processing

--- tag-seq.R
+++ tag-seq.R
@@ -5,6 +5,8 @@
 
 # Load packages -----------------------------------------------------------
 
+setwd("/Users/blaufer/Box Sync/PEBBLES")
+
 rm(list=ls())
 options(scipen=999)
 
@@ -13,18 +15,60 @@ if (!requireNamespace("BiocManager", quietly=TRUE))
 #BiocManager::install(c("edgeR", "biobroom"))
 stopifnot(suppressMessages(sapply(c("edgeR", "biobroom", "tidyverse"), require, character.only = TRUE)))
 
-# Load data ---------------------------------------------------------------
-
-setwd("/Users/blaufer/Box Sync/PEBBLES")
-meta <- read.csv("sample_info.csv")
-files <- list.files(path = glue::glue(getwd(), "/GeneCounts"), pattern = "*.ReadsPerGene.out.tab")
-
-# Format count matrix -----------------------------------------------------
+# Count Matrix ------------------------------------------------------------
 
 #name <- gsub( "(?:[^_]+_){4}([^_ ]+)*$","", files)
 
-name <- str_split_fixed(files, "_", n =4) %>%
-  as.tibble() %>%
-  unite(Name, c(V1:V3), sep = "_") %>%
-  select(Name)
+# STAR quantMode geneCounts output:
+#column 1: gene ID
+#column 2: counts for unstranded RNA-seq
+#column 3: counts for the 1st read strand aligned with RNA (htseq-count option -s yes)
+#column 4: counts for the 2nd read strand aligned with RNA (htseq-count option -s reverse)
+
+sampleNames <- list.files(path = glue::glue(getwd(), "/GeneCounts"), pattern = "*.ReadsPerGene.out.tab") %>%
+  stringr::str_split_fixed("_", n = 4) %>%
+  as_tibble() %>%
+  tidyr::unite(Name, c(V1:V3), sep = "_") %>%
+  dplyr::select(Name) %>% 
+  purrr::flatten_chr()
+
+ensemblIDs <- list.files(path = glue::glue(getwd(), "/GeneCounts"), pattern = "*.ReadsPerGene.out.tab", full.names = T)[1] %>% 
+  data.table::fread(select = 1) %>%
+  purrr::flatten_chr()
+
+countMatrix <- list.files(path = glue::glue(getwd(), "/GeneCounts"), pattern = "*.ReadsPerGene.out.tab", full.names = T) %>%
+  purrr::map_dfc(data.table::fread, select = 3, data.table = F) %>%
+  magrittr::set_colnames(sampleNames) %>% 
+  magrittr::set_rownames(ensemblIDs) %>%
+  as.matrix()
+  
+countMatrix <- countMatrix[-c(1:4),]
+
+# Design Matrix -----------------------------------------------------------
+
+designMatrix <- read.csv("sample_info.csv") %>%
+  as_tibble()
+
+designMatrix$Name <- gsub("-", "_", designMatrix$Name)
+designMatrix$Name <- gsub(" ", "_", designMatrix$Name)
+designMatrix$Name <- gsub("\n", "", designMatrix$Name)
+
+# Preprocessing -----------------------------------------------------------
+
+# Calculate normalization factors
+countMatrix <- countMatrix %>%
+  DGEList() %>%
+  calcNormFactors()
+
+# Filter genes with low expression
+cutoff <- 1
+drop <- which(apply(cpm(countMatrix), 1, max) < cutoff)
+countMatrix <- countMatrix[-drop,] 
+dim(countMatrix) 
+
+# Group information
+stopifnot(rownames(countMatrix$samples) == designMatrix$Name)
+
+plotMDS(countMatrix)
+
 
